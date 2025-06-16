e8ea32b42d00121f05df1b2ff5f2885b696ee300
CodeJr10 <aadityamalap666@gmail.com>
2025-06-16 23:50:37 +0530

    feat: Implement hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="">
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="/hero.png" alt="Hero Banner" />
+          <h1 className="">
+            Find Your Favorite <span className="text-gradient">Movies</span>{" "}
+          </h1>
+        </header>
+        <p className="">Search</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </main>
+  );
+};
 
-export default App
+export default App;
