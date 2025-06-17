1d933699f28be7e0cacc71ea240fe90f3d8caa8a
CodeJr10 <aadityamalap666@gmail.com>
2025-06-17 20:40:01 +0530

    feat: Implement fetch Api

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Search from "./components/Search";
 
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
+
 const App = () => {
   const [searchInput, setSearchInput] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const fetchMovies = async () => {
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+      setErrorMessage("Failed to fetch movies. Please try again later.");
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
   return (
     <main className="">
       <div className="pattern" />
@@ -17,6 +47,8 @@ const App = () => {
         <div className="">
           <Search searchInput={searchInput} setSearchInput={setSearchInput} />
         </div>
+
+        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
       </div>
     </main>
   );
